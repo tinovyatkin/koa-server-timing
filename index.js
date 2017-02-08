221258ad8c811dbcc6f9061516e9eae629900ca4
tinovyatkin <tino@vtkn.io>
2017-02-08 17:46:06 -0400

    removed sluggify dependency

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const slug = require('slugify');
 const assert = require('assert');
+
+function getSlug(str) {
+  if (typeof str !== 'string') return '';
+  return str.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^a-z-]/g, '');
+}
 
 module.exports = ({ total } = { total: process.env.NODE_ENV !== 'production' }) => async (ctx, next) => {
   // attaching timings object to state
   ctx.state.timings = {
     all: new Map(),
-    startSpan(spanName) {
-      const key = slug(spanName);
-      console.log(key);
-      assert.ok(!this.all.has(key), 'This span is running already, name muse be unique!');
-      this.all.set(key, { start: process.hrtime(), desc: spanName });
-      return key;
+    startSpan(spanDesc, spanSlug = getSlug(spanDesc)) {
+      assert.ok(!this.all.has(spanSlug), 'This span is running already, name muse be unique!');
+      assert.ok(spanSlug.length, 'Either slug or description must be non-empy');
+      this.all.set(spanSlug, { start: process.hrtime(), desc: spanDesc || '' });
+      return spanSlug;
     },
     stopSpan(spanName) {
-      const key = slug(spanName);
+      const key = this.all.has(spanName) ? spanName : getSlug(spanName);
       assert.ok(this.all.has(key), 'Span to stop is not found!');
       assert.ok('start' in this.all.get(key), 'Span to stop were never started!');
       const timing = this.all.get(key);
@@ -33,7 +36,7 @@ module.exports = ({ total } = { total: process.env.NODE_ENV !== 'production' }) 
   // constructing headers array
   const metrics = [];
   for (const [key, { stop: [ sec, nanosec ], desc }] of ctx.state.timings.all) {
-    metrics.push(`${key}=${((sec * 1e9 + nanosec) / 1000000).toFixed(1)}${key !== desc ? `; "${desc}"` : ''}`);
+    metrics.push(`${key}=${((sec * 1e9 + nanosec) / 1000000).toFixed(1)}${desc.length && key !== desc ? `; "${desc}"` : ''}`);
   }
 
   // Adding our headers now
